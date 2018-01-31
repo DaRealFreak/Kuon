2d05dad93cdaeba7e052b6987eff73b2c47fd8a4
Steffen Keuper <steffen.keuper@web.de>
2018-01-31 17:01:32 +0100

    [BUGFIX] fix recursion of LockedList and LockedDict for APIResponse

--- opskins/api/api_response.py
+++ opskins/api/api_response.py
@@ -1,29 +1,103 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import collections
 import json
 
-from opskins.api.exceptions import InvalidApiResponseType
+from opskins.api.exceptions import *
+
+from collections import MutableSequence
 
 
-class APIResponse(collections.Mapping):
+class LockedList(MutableSequence):
+    """A container for manipulating lists of hosts"""
+
+    def __init__(self, data=None):
+        """Initialize the class"""
+        super(LockedList, self).__init__()
+        if data is not None:
+            self._list = list(data)
+        else:
+            self._list = list()
+
+    def __repr__(self):
+        return str(self._list)
+
+    def __len__(self):
+        return len(self._list)
+
+    def __getitem__(self, ii):
+        val = self._list[ii]
+        if isinstance(val, list) or isinstance(val, tuple):
+            val = LockedList(val)
+        if isinstance(val, dict):
+            val = LockedDict(val)
+        return val
+
+    def __delitem__(self, ii):
+        del self._list[ii]
+
+    def __setitem__(self, ii, val):
+        pass
+
+    def __str__(self):
+        return str(self._list)
+
+    def insert(self, ii, val):
+        pass
+
+    def append(self, val):
+        pass
+
+
+class LockedDict(dict):
+    """
+    Custom dict where customizing values isn't possible by default
+    Return LockedList on list or tuple types
+    """
+
+    def __init__(self, *args, **kwargs):
+        super(LockedDict, self).__init__(*args, **kwargs)
+        for arg in args:
+            if isinstance(arg, dict):
+                for k, v in arg.items():
+                    self[k] = v
+
+        if kwargs:
+            for k, v in kwargs.items():
+                self[k] = v
+
+    def __getattr__(self, attr):
+        val = self.get(attr)
+        if isinstance(val, list) or isinstance(val, tuple):
+            val = LockedList(val)
+        if isinstance(val, dict):
+            val = LockedDict(val)
+        return val
+
+    def __setattr__(self, key, value):
+        pass
+
+    def __setitem__(self, key, value):
+        pass
+
+    def __delattr__(self, item):
+        self.__delitem__(item)
+
+    def __delitem__(self, key):
+        super(LockedDict, self).__delitem__(key)
+        del self.__dict__[key]
+
+
+class APIResponse(LockedDict):
+    """
+    Wrapper for the json response returned from the OPSkins API
+    Uses LockedDict and LockedList classes to prevent modifications to the retrieved values
+    """
 
     def __init__(self, data):
         try:
-            self._data = json.loads(data)
+            data = json.loads(data)
         except ValueError:
             raise InvalidApiResponseType("The API didn't return a valid json format")
-        self._data = data
 
-    def __repr__(self):
-        return self._data
-
-    def __getitem__(self, key):
-        return self._data[key]
-
-    def __len__(self):
-        return len(self._data)
-
-    def __iter__(self):
-        return iter(self._data)
+        super().__init__(data)
