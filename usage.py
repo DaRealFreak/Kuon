d57ccf4360a1fb510bba0d44167eb154c5da241e
Steffen Keuper <steffen.keuper@web.de>
2018-02-04 22:34:35 +0100

    [TASK] extend usage by search_no_delay function

--- usage.py
+++ usage.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 #
 # This is an example usage
-
 from pprint import pprint
 
 from opskins.api.common import *
@@ -26,7 +25,12 @@ if __name__ == '__main__':
     pprint(user_interface.get_balance())
 
     sales_interface = ISales()
-    pprint(sales_interface.get_sales(status_type=ItemStatus.SOLD_AND_DELIVERED, app_id=CommonSteamGames.APP_ID_CSGO))
+    pprint(sales_interface.get_sales(status_type=ItemStatus.SOLD_AND_DELIVERED,
+                                     app_id=CommonSteamGames.APP_ID_CSGO))
     pprint(sales_interface.get_last_sales(app_id=CommonSteamGames.APP_ID_CSGO, context_id=ContextIds.VALVE_GAMES,
                                           market_name='AK-47 | Aquamarine Revenge (Field-Tested)'))
-    pprint(sales_interface.search(search_word="howl min", app_id=CommonSteamGames.APP_ID_CSGO))
+    pprint(sales_interface.search(search_item="howl min", app_id=CommonSteamGames.APP_ID_CSGO))
+
+    # uses selenium chrome driver so initial call takes a few seconds to initialise the driver
+    # on second use and later it will take way less time
+    pprint(sales_interface.search_no_delay(search_item="howl min", app_id=CommonSteamGames.APP_ID_CSGO))
