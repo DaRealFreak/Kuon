23d6d98ceb1332af9119def9fbd650e2e1be5a74
Steffen Keuper <steffen.keuper@web.de>
2018-02-05 23:25:47 +0100

    [TASK] implement basic tracker structure

--- opskins/watcher/tracker/tracker.py
+++ opskins/watcher/tracker/tracker.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import logging
+
+from opskins.api.api_response import LockedDict, LockedList
+from opskins.watcher.tracker.json_manager import JsonManager
 
 
 class TrackConditions:
@@ -12,5 +16,45 @@ class TrackConditions:
 class Tracker:
     """Class to track search terms with conditions"""
 
-    def __init__(self):
-        pass
+    ITEM_REQUIRED_KEYWORDS = ('search_item', 'value', 'unit', 'condition')
+
+    def __init__(self, log_level=logging.ERROR):
+        """Initializing function"""
+        logging.basicConfig(level=log_level)
+        self.logger = logging.getLogger("opskins_tracker")
+
+        self._json_manager = JsonManager()
+        self._tracked_items = self._json_manager.get_tracked_items(self.ITEM_REQUIRED_KEYWORDS)
+
+    @property
+    def tracked_items(self):
+        """Get the tracked items as LockedList
+
+        :return:
+        """
+        return LockedList(self._tracked_items)
+
+    def add_item(self, item: LockedDict):
+        """Adds an item to the tracking list
+
+        :param item:
+        :return:
+        """
+        if not all([key in item for key in Tracker.ITEM_REQUIRED_KEYWORDS]):
+            self.logger.error('Not all required keywords ({keywords}) are in the added item'.format(
+                keywords=",".join(Tracker.ITEM_REQUIRED_KEYWORDS)))
+            return
+
+        if item not in self._tracked_items:
+            self._tracked_items.append(item)
+            self._json_manager.save_tracked_items(self._tracked_items)
+
+    def remove_item(self, item: LockedDict):
+        """Removes an item from the tracking list
+
+        :param item:
+        :return:
+        """
+        if item in self._tracked_items:
+            self._tracked_items.remove(item)
+            self._json_manager.save_tracked_items(self._tracked_items)
