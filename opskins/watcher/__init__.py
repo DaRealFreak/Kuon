b8a3e6b4a3cf4bfe91e260c0d617dffe376d64a8
Steffen Keuper <steffen.keuper@web.de>
2018-02-05 09:54:40 +0100

    [BUGFIX] import settings before importing the watcher

--- opskins/watcher/__init__.py
+++ opskins/watcher/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from opskins.watcher.watcher import Watcher
 from opskins.watcher.settings import Settings
+from opskins.watcher.watcher import Watcher
 
 __all__ = [Watcher, Settings]
