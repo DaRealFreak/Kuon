4d76914bb88180ab3b02bfbf49e866a9dccafd7d
Steffen Keuper <steffen.keuper@web.de>
2018-02-05 11:18:25 +0100

    [TASK] add functional test for mail notifications

---
+++ tests/watcher/notifications/test_mail.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from opskins.watcher.notifications.mail import Mail
+
+if __name__ == "__main__":
+    mail = Mail()
+    mail.send_message('Mail Test', '<a href="https://imgur.com/a/Cdvt5">2B Nier Automata imgur album</a>')
