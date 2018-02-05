aeb8849036c205c3dcfc9f13aabb0f6f36e6cb2e
Steffen Keuper <steffen.keuper@web.de>
2018-02-05 23:26:10 +0100

    [TASK] make keywords consistent to API parameters

--- tests/watcher/tracker/test_json_manager.py
+++ tests/watcher/tracker/test_json_manager.py
@@ -7,13 +7,13 @@ from opskins.watcher.tracker.json_manager import JsonManager
 if __name__ == '__main__':
     example_items = [
         {
-            'search_word': "m4 howl min",
+            'search_item': "m4 howl min",
             'value': 80000,
             'unit': '#',
             'condition': TrackConditions.BELOW_VALUE
         },
         {
-            'search_word': "m4 howl min",
+            'search_item': "m4 howl min",
             'value': 10,
             'unit': '%',
             'condition': TrackConditions.BELOW_AVERAGE
