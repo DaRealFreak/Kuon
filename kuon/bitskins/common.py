ec3efb650c012cb5371fcfad6d1d80a9cc2ce177
Steffen Keuper <steffen.keuper@web.de>
2018-02-14 11:18:31 +0100

    [TASK] add BitSkins specific settings

---
+++ kuon/bitskins/common.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+class Sorting:
+    CREATED_AT = "created_at"
+    PRICE = "price"
+
+
+class SortingDirection:
+    DESCENDING = "desc"
+    ASCENDING = "asc"
+
+
+class Processor:
+    PAYPAL = "paypal"
+    BITCOIN = "bitcoin"
+    BANK_WIRE = "bank wire"
