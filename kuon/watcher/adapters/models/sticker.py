8ed15390ee47c9ff468c1c21c059046bc732e725
Steffen Keuper <steffen.keuper@web.de>
2018-02-17 02:20:45 +0100

    [TASK] set AbstractEntity as parent

--- kuon/watcher/adapters/models/sticker.py
+++ kuon/watcher/adapters/models/sticker.py
@@ -1,23 +1,31 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+from kuon.watcher.adapters.models import AbstractEntity
 
-class Sticker:
+
+class Sticker(AbstractEntity):
+    """General Sticker Class"""
+
     def __init__(self, name: str, image: str, wear_value: float):
+        """Initializing function
+
+        :param name:
+        :param image:
+        :param wear_value:
+        """
         self._name = name
         self._image = image
         self._wear_value = wear_value
 
-    def __dict__(self):
-        return self.value
-
     @property
     def value(self):
+        """Return all important information regarding the Stickers
+
+        :return:
+        """
         return {
             'name': self._name,
             'image': self._image,
             'wear_value': self._wear_value
         }
-
-    def __repr__(self):
-        return str(self.value)
