89f9fcabf12f888159e3be214dd38d354e9ee8b4
Steffen Keuper <steffen.keuper@web.de>
2018-02-19 12:45:24 +0100

    [TASK] include implemented adapters in __all__

---
+++ kuon/watcher/adapters/bitskins/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from kuon.watcher.adapters.bitskins.pricing_adapter import PricingAdapter
+from kuon.watcher.adapters.bitskins.sales_adapter import SalesAdapter
+
+__all__ = [
+    PricingAdapter.__name__,
+    SalesAdapter.__name__
+]
