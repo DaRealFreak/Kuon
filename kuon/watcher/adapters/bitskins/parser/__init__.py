201eca3fb3279110aa2dc2916fdcdb7f996b08f8
Steffen Keuper <steffen.keuper@web.de>
2018-02-20 13:39:33 +0100

    [TASK] add SoldHistoryParser to __all__

--- kuon/watcher/adapters/bitskins/parser/__init__.py
+++ kuon/watcher/adapters/bitskins/parser/__init__.py
@@ -2,7 +2,9 @@
 # -*- coding: utf-8 -*-
 
 from kuon.watcher.adapters.bitskins.parser.search_response import SearchResponseParser
+from kuon.watcher.adapters.bitskins.parser.sold_history import SoldHistoryParser
 
 __all__ = [
-    SearchResponseParser.__name__
+    SearchResponseParser.__name__,
+    SoldHistoryParser.__name__
 ]
