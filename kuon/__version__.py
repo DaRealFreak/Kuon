92e1e3636560e1585fbabdde0913941dd2635bcf
Steffen Keuper <steffen.keuper@web.de>
2018-02-22 02:19:02 +0100

    [TASK] update project url, push version to 2.0.0

--- kuon/__version__.py
+++ kuon/__version__.py
@@ -4,8 +4,8 @@
 __title__ = 'Kuon'
 __description__ = 'Bot to watch Prices from OPSkins and Bitskins and notify the user. Name or the projects ' \
                   'originates from the anime Utawarerumono: Itsuwari no Kamen'
-__version__ = '1.0.0'
-__url__ = 'https://github.com/DaRealFreak/opskins-bot'
+__version__ = '2.0.0'
+__url__ = 'https://github.com/DaRealFreak/Kuon'
 __author__ = 'DaRealFreak'
 __author_email__ = 'steffen.keuper@web.de'
 __license__ = 'MIT'
