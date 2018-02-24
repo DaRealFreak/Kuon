ccf92213ac8abb10bb26ec5528b4b03e6571e593
Steffen Keuper <steffen.keuper@web.de>
2018-02-24 09:50:16 +0100

    [TASK] only iterate through items in results if success = True for BitSkins

--- kuon/watcher/adapters/bitskins/parser/search_response.py
+++ kuon/watcher/adapters/bitskins/parser/search_response.py
@@ -19,26 +19,29 @@ class SearchResponseParser:
         :param results:
         :return:
         """
-        response = SearchResponse(success=results['status'] == 'success')
-        for item in results['data']['items']:
-            wear = item['float_value']
-            if wear is None:
-                wear = -1.0
+        success = results['status'] == 'success' and 'items' in results['data']
+        response = SearchResponse(success=success)
 
-            item_model = Item(
-                market_name=item['market_hash_name'],
-                item_id=int(item['item_id']),
-                app_id=int(item['app_id']),
-                class_id=int(item['class_id']),
-                context_id=int(item['context_id']),
-                instance_id=int(item['instance_id']),
-                price=int(float(item['price']) * 100),
-                wear_value=float(wear),
-                image=item['image'],
-                inspect_link=item['inspect_link'],
-                stickers=SearchResponseParser.get_stickers(item['stickers'])
-            )
-            response.add_item(item_model)
+        if success:
+            for item in results['data']['items']:
+                wear = item['float_value']
+                if wear is None:
+                    wear = -1.0
+
+                item_model = Item(
+                    market_name=item['market_hash_name'],
+                    item_id=int(item['item_id']),
+                    app_id=int(item['app_id']),
+                    class_id=int(item['class_id']),
+                    context_id=int(item['context_id']),
+                    instance_id=int(item['instance_id']),
+                    price=int(float(item['price']) * 100),
+                    wear_value=float(wear),
+                    image=item['image'],
+                    inspect_link=item['inspect_link'],
+                    stickers=SearchResponseParser.get_stickers(item['stickers'])
+                )
+                response.add_item(item_model)
         return APIResponse(json.dumps(response.__dict__))
 
     @staticmethod
