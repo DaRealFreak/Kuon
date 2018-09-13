085784d463871bd9f9e133f9a3ec116be440f89e
Steffen Keuper <steffen.keuper@web.de>
2018-09-13 18:14:06 +0200

    [TASK] add common steam urls

---
+++ kuon/steam/common.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+class SteamUrls:
+    COMMUNITY = "https://steamcommunity.com"
+    STORE = 'https://store.steampowered.com'
+
+    API = "https://api.steampowered.com"
